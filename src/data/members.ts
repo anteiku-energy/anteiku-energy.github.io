d29a346839b8127a0204e1cfc6816f21270a3b49
Nayara <zdelucuaffg@gmail.com>
2026-07-02 20:40:13 -0300

    Fix typo in member bio for 'nayara / nay'

--- src/data/members.ts
+++ src/data/members.ts
@@ -33,7 +33,7 @@ export const members: Member[] = [
   },
   {
     nick: "nayara / nay",
-    bio: "Needa slow down its not a rece, can this feeling be erased?",
+    bio: "Needa slow down its not a race, can this feeling be erased?",
   },
   {
     nick: "rovier",
